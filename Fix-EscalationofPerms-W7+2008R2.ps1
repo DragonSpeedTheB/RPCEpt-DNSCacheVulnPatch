6874594c3bb95375ffbf4e770a713111d5891cd2
DragonSpeedTheB <47401585+DragonSpeedTheB@users.noreply.github.com>
2020-12-08 09:50:52 -0800

    Update Fix-EscalationofPerms-W7+2008R2.ps1
    Original was setting permissions on the PARENT key.  Removing User READ perms from the parent key resulted in a pretty unhappy system :(

--- Fix-EscalationofPerms-W7+2008R2.ps1
+++ Fix-EscalationofPerms-W7+2008R2.ps1
@@ -1,4 +1,4 @@
-﻿$Usersid = New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-545'
+$Usersid = New-Object System.Security.Principal.SecurityIdentifier 'S-1-5-32-545'
 $Everyonesid = New-Object System.Security.Principal.SecurityIdentifier 'S-1-1-0'
 $user = $Usersid.Translate([System.Security.Principal.NTAccount]).Value
 $everyone = $Everyonesid.Translate([System.Security.Principal.NTAccount]).Value
@@ -13,16 +13,27 @@ foreach ($aclpath in $aclpaths)
     {
         New-Item $aclpath -Name Performance |Out-Null
     }
-    $acl = get-acl $aclpath
+    $acl = get-acl $perfpath
     # disable permission inheritance and copy existing permissions to explicit permissions on parent Key
     $acl.SetAccessRuleProtection($true,$true)
     # write the changes back to the reg key
-    $acl | Set-Acl -Path $aclpath
+    $acl | Set-Acl -Path $perfpath
     # get the new (inheritance disabled) permission structure
-    $acl = get-acl $aclpath
+    $acl = get-acl $perfpath
     # remove any rule that applies to Users
     $acl.PurgeAccessRules([System.Security.Principal.NTAccount]$user)
     $acl.PurgeAccessRules([System.Security.Principal.NTAccount]$everyone)
     # write the changes 
+    $acl | Set-Acl -Path $perfpath
+    # fix Parent ACL
+    $acl = get-acl $aclpath
+    # Enale Inheritance on the parent key
+    $acl.SetAccessRuleProtection($false,$true)
+    # Ensure that Users have ReadKey privs on the parent
+    $AccessRule = New-Object System.Security.AccessControl.RegistryAccessRule($user,[System.Security.AccessControl.RegistryRights]::ReadKey,"Allow")
+    $acl.SetAccessRule($AccessRule)
+    # write the changes 
     $acl | Set-Acl -Path $aclpath
 }
+
+
